c008da76a50944685529cae3214c5c1b66bfc811
DEVIKA MANOJ C P <devikamanojcp2004@gmail.com>
2025-11-05 10:48:56 +0530

    function() questions

---
+++ function.js
@@ -0,0 +1,50 @@
+//FUNCTION QUESTIONS
+//1.Write a function square(num) that returns the square of a number.
+// function square(num){
+//     return num*num;
+// }
+// console.log(square(7))
+
+
+//2.Write a function greet(name) that returns "Hello, <name>!"
+// function greet(name){
+//     return "hello "+ name;
+// }
+// console.log(greet("devika"));
+
+//3.Write a function add(a, b, c) that returns the sum of three numbers.
+// function add(a,b,c){
+//     return a+b+c;
+// }
+// console.log(add(10,80,10));
+
+//4.Write a function isEven(num) that returns true if the number is even, else false.
+
+//method1
+
+// function isEven(num){
+//     if(num%2==0){
+//         console.log("true");
+//     }else{
+//         console.log("false");
+        
+//     }
+// }isEven(5)
+
+//method 2
+
+// function isEven(num){
+//     return num%2==0;
+// }
+// console.log(isEven(6));
+
+
+//5.Write a function getFullName(first, last) that returns the full name joined with a space.
+// function getFullName(first, last){
+//     return first + "  "+ last ;
+// }
+// console.log(getFullName("Devika","Manoj"));
+
+
+
+
